a34bd46170f0956e7f6f23d0f51307916b735caf
David St John <712020:dc17e004-6d3e-42cd-8fdf-0f8f79207061@bots.bitbucket.org>
2023-07-02 16:46:52 -0700

    added date comparison for ami's greater then 30 days, patch and lifecycle compliance.

--- get_aws_marketplace_amis/get_linux_amis.py
+++ get_aws_marketplace_amis/get_linux_amis.py
@@ -2,6 +2,7 @@ import boto3
 import re
 import sys
 import pandas as pd
+import datetime
 
 def list_free_linux_amis(name_pattern):
     '''
@@ -43,18 +44,44 @@ def list_free_linux_amis(name_pattern):
     # Save results to a text file
     with open('ami_byname.txt', 'w') as file:
         for ami in ami_data:
+            # assign ami['Build Date'] value for the build date of an AMI
+            build_date_str = ami['Build Date']
+
+            # Convert the build date string to a datetime object
+            build_date = datetime.datetime.strptime(build_date_str, "%Y-%m-%dT%H:%M:%S.%fZ")
+
+            # Set our current date
+            current_date = datetime.datetime.now()
+
+            # Calculate the time difference between the current date and the build date
+            time_difference = current_date - build_date
+
+            # Check if the time difference is greater than 30 days and notify of patch management
+            if time_difference.days > 30:
+                print("WARNING: AMI > than 30 days. for security requirements, run updates.\n")
+                file.write(f"PATCH NOTICE: AMI > than 30 days. for security requirements, run updates.\n")
+            else:
+                print("AMI is CURRENT!\n")
+                file.write(f"AMI SECURITY STATUS: last release was less then 30 days ago, may still need updated.\n")
+            print(f"Searching by regex pattern: {regex_pattern}")
+            file.write(f"Found AMI with pattern: {regex_pattern}\n")
             file.write(f"Provider: {ami['Provider']}\n")
+            print(f"Provider: {ami['Provider']}\n")
             file.write(f"AMI ID: {ami['AMI ID']}\n")
+            print(f"AMI ID: {ami['AMI ID']}\n")
             file.write(f"Name: {ami['Name']}\n")
+            print(f"Name: {ami['Name']}\n")
             file.write(f"Build Date: {ami['Build Date']}\n")
+            print(f"Build Date: {ami['Build Date']}\n")
             file.write("--------------------\n")
+            print("#------------------------\n")
 
     # Generate a spreadsheet report
     df = pd.DataFrame(ami_data)
     df.to_excel('ami_byname_report.xlsx', index=False)
 
 # Specify the name regex pattern as a command-line parameter or use a default value
-#name_regex = r'(CentOS|Red Hat|Fedora|Almalinux|Rocky Linux)'
+# name_regex = r'(CentOS|Red Hat|Fedora|Almalinux|Rocky Linux)'
 
 # Uncomment the line below to specify the name regex pattern as a command-line parameter
 name_regex = sys.argv[1]
